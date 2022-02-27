d1a454a58fdd7bf5472644bbb7f27ea86d226776
Marco Kang <kmnkit@gmail.com>
2022-02-27 19:47:07 +0900

    slug 생성 tutorial

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # django-korean-slug-test
+
 한국어 Slug Field test를 위한 연습용 프로젝트
+
+## 사용방법
+
+1. Object 생성
+2. 생성한 Object의 save method를 사용하면 slug가 생성됨.
--- posts/models.py
+++ posts/models.py
@@ -4,12 +4,11 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, allow_unicode=True)
 
     def __str__(self):
         return self.title
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
+        self.slug = slugify(self.title, allow_unicode=True)
         super(Post, self).save(*args, **kwargs)
