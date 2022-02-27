9d4132820b95cfee1952e7642fb96d681acd6b78
Marco Kang <kmnkit@gmail.com>
2022-02-27 18:42:23 +0900

    slug field를 포함하는 모델 간단하게 정의

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
+from django.utils.text import slugify
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
